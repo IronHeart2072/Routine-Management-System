65a0271e52b0823ba56eb05081642e0ba519ba65
IronHeart2072 <king_2.2@outlook.com>
2019-08-09 12:29:26 +0545

    Update CMS

--- cms/login.php
+++ cms/login.php
@@ -1,53 +1,89 @@
-<?php
-    session_start();
-    require_once('connect.php');
-    
-        if(isset($_POST['login']))
-        {
-            if(empty($_POST['username']) || empty($_POST['password']) )
-            {
-                header("location:logindesign.php?empty");
-                exit();
-            }
-            else
-            {
-                $Username = mysqli_real_escape_string($con,$_POST['username']);
-                $Password = mysqli_real_escape_string($con,$_POST['password']);
-    
-                $sql = " SELECT * FROM users WHERE username = '$Username' AND password = '$Password' ";
-                $result = mysqli_query($con,$sql);
-                if (mysqli_num_rows($result) == 1) 
-                {
-                    
-                    if($Password==false)
-                    {
-                        header("location:../index.php?P_Invalid");
-                        exit();
-                    
-                        
-                    
-                    }
-                    elseif($Password==true)
-                    {
-                        // $_SESSION['U_D']=$row['id'];
-                        // $_SESSION['Username']=$row['username'];
-                        // $_SESSION['Password']=$row['password'];
-     
-                        header("location:admindash.php?Well");
-                        exit();
-                        
-     
-                    }
-     
-                }
-                else
-                {
-                    header("location:logindesign.php?U_Invalid");
-                    exit();
-                }
-                
-        }
-    }
-            
-    
+<?php 
+   session_start();
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once("header.php"); 
+
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once("class.ManageUsers.php");
+   
+   $users = new ManageUsers();
+   
+   
+   if(isset($_POST['lgn']))
+	{
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		$count = $users->LoginUsers($username, $password);
+		if($count ==0)
+		{
+			echo "You are not yet Registered";
+		}
+		else if($count == 1)
+		{
+			$make_sessions = $users->GetUserInfo($username);
+				foreach($make_sessions as $userSessions)
+				{
+					$_SESSION['name'] = $userSessions['username'];
+					if(isset($_SESSION['name']))
+					{
+						header("location: ../dashboard/dashboard.php");
+					}
+				}
+		}
+		
+	}
+	
+	
+?>
+<body>
+	<nav class="navbar navbar-default navbar-static-top">
+	  <div class="container">
+	  <h3>Routine Generator</h3>
+	  </div>
+	</nav>
+	
+	<div id="content">
+		<div id="form">
+		<form class="form-horizontal" method="post" action="">
+			<fieldset>
+
+			<!-- Form Name -->
+			<legend>Login Here</legend>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="username">Username</label>  
+			  <div class="col-md-4">
+			  <input id="username" name="username" type="text" placeholder="" class="form-control input-md" required="">
+				
+			  </div>
+			</div>
+
+			<!-- Password input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="password">Password</label>
+			  <div class="col-md-4">
+				<input id="password" name="password" type="password" placeholder="" class="form-control input-md" required="">
+				
+			  </div>
+			</div>
+
+			<!-- Button -->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="login"></label>
+			  <div class="col-md-4">
+				<input type="submit" name="lgn" class="btn btn-success" value="Login">
+			  </div>
+			</div>
+
+			</fieldset>
+		</form>
+		</div>
+	</div>
+
+</body>
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   include_once("../cms/footer.php");
 ?>
